afb4068977ba68d070b4735ea654ea5beab2be8f
Diegoni <diego_nieto_1@hotmail.com>
2016-01-25 15:57:50 -0300

    1.1.7

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-	$username="root";
-	$password="bluepill";
-	$database="estacionamiento";
-	$url="localhost";
+	$username	= "root";
+	$password	= "bluepill";
+	$database	= "estacionamiento";
+	$url		= "localhost";
 	mysql_connect($url,$username,$password);
 	@mysql_select_db($database) or die( "No pude conectarme a la base de datos");
 ?>
